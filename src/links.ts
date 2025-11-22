5958b5270db1dc6d6440f64a58300395c6e13b2a
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2025-11-22 12:33:19 +0800

    更新pv1地址

--- src/links.ts
+++ src/links.ts
@@ -1,9 +1,6 @@
 export const LINK_SUBSCRIBE_LIVE = "https://space.bilibili.com/660303135"
 export const LINK_VIDEO_PV0 = "https://www.bilibili.com/video/BV1cZsbzeEyv/"
-/**
- * TODO: 填写 PV1 视频地址
- */
-export const LINK_VIDEO_PV1 = ""
+export const LINK_VIDEO_PV1 = "https://www.bilibili.com/video/BV1HMUnBEEtM/"
 export const LINK_GIFT_LINK = "https://m.tb.cn/h.SE6KoDF?tk=r5ebfOHO56I"
 export const LINK_BLESS = "https://www.bilibili.com/opus/1137940233534308353"
 export const LINK_BILIBILI_ACCOUNT = "https://space.bilibili.com/660303135"
