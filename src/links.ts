b0702da8bd328ff75a74a624cf354f74ccc1ff56
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2025-11-22 12:35:04 +0800

    更新预约直播地址

--- src/links.ts
+++ src/links.ts
@@ -1,4 +1,4 @@
-export const LINK_SUBSCRIBE_LIVE = "https://space.bilibili.com/660303135"
+export const LINK_SUBSCRIBE_LIVE = "https://www.bilibili.com/opus/1137940233534308353"
 export const LINK_VIDEO_PV0 = "https://www.bilibili.com/video/BV1cZsbzeEyv/"
 export const LINK_VIDEO_PV1 = "https://www.bilibili.com/video/BV1HMUnBEEtM/"
 export const LINK_GIFT_LINK = "https://m.tb.cn/h.SE6KoDF?tk=r5ebfOHO56I"
