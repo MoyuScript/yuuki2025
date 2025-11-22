a4f84979b508713c34a8cb3e88d18e3da46dfd0a
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2025-11-22 12:29:51 +0800

    B站动态地址更新

--- src/links.ts
+++ src/links.ts
@@ -5,8 +5,5 @@ export const LINK_VIDEO_PV0 = "https://www.bilibili.com/video/BV1cZsbzeEyv/"
  */
 export const LINK_VIDEO_PV1 = ""
 export const LINK_GIFT_LINK = "https://m.tb.cn/h.SE6KoDF?tk=r5ebfOHO56I"
-/**
- * TODO: 填写 B 站动态地址
- */
-export const LINK_BLESS = ""
+export const LINK_BLESS = "https://www.bilibili.com/opus/1137940233534308353"
 export const LINK_BILIBILI_ACCOUNT = "https://space.bilibili.com/660303135"
